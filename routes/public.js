546bb9b51941601994c7cb2699d326fc3d2324c2
DESKTOP-89B4AMI\alexa <alexandresousa.eng@gmail.com>
2025-07-11 23:54:58 -0300

    feature: cadastrar
    Adicionada operação de cadastro de livros
    
    LIV-1

---
+++ routes/public.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+//cadastrar-livro
+router.post('/cadastro', async (req, res) => {
+
+    try {
+        const user = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(user.password, salt);
+        
+        const userDB = await prisma.User.create({
+            data: {
+                user: user.user,
+                password: hashPassword,
+                book: user.book,
+                author: user.author,
+                notes: user.notes,
+                done: user.done
+            }
+        })
+
+        res.status(201).json(userDB);
+    } catch (error) {
+        res.status(500).json({message: 'server error'});
+    }
+    
+})
+
+export default router;
